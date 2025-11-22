050f3b53fe858a73e6e6dd5ac0afd8345d95f4a5
Ayoub jalyta <ayoubcode941@gmail.com>
2025-11-22 18:16:42 +0100

    Add project and task factories with seeder logic

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Project;
+use App\Models\Task;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +17,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+       
+    
+    User::factory(5) ->has(Project::factory()->count(3)->has(Task::factory()->count(5)))->create();
+      
     }
 }
