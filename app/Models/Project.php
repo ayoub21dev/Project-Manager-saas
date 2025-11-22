d12187995eb9bc233bda33ac9246fea34cea6aa5
Ayoub jalyta <ayoubcode941@gmail.com>
2025-11-22 17:44:03 +0100

    Add Project and Task models with relationships and fillable attributes

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Project extends Model
 {
-    //
+   use HasFactory;
+   
+     protected $fillable = ['title', 'description', 'status', 'user_id']; 
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+
+   
+
+
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Project;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Task extends Model
 {
-    //
+     use HasFactory;
+    protected $fillable = ['title', 'is_completed', 'project_id'];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
